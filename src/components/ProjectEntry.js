c151243e4547c4f2c1285482dd300e1beb9439f4
2CARUS <c.kopp.professional@gmail.com>
2019-02-11 15:53:42 -0600

    initial commit

---
+++ src/components/ProjectEntry.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const optionConfig = {};
+
+class ProjectEntry extends React.Component {
+  render() {
+    return <div>I'll keep on trying...might as well</div>;
+  }
+}
+
+export default ProjectEntry;
